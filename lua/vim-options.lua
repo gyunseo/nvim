36ec6edcc65f75b4920edca870e5645cc492aade
Gyunseo Lee <rbstj0311@naver.com>
2024-01-26 20:31:03 +0900

    neovim configuration using luascript

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.wo.number = true
+vim.g.mapleader = " "
+vim.api.nvim_exec('language en_US', true)
+vim.api.nvim_set_option("clipboard","unnamed") -- to use OS clipboard 
