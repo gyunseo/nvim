04fa7da8ed42865d7fa7b85c143d77bad2b79dd1
Gyunseo Lee <rbstj0311@naver.com>
2024-01-27 23:20:04 +0900

    chore: fix neo tree lua script to show hiden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,13 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          }
+        }
+      }
       vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
     end
   }
