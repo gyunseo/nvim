fe8f5b9650ceb58a16ec4700498d8065aa365e43
Gyunseo Lee <rbstj0311@naver.com>
2024-02-01 02:54:17 +0900

    fix: change init.lua script to resolve vscode error
    vscode에서 os clipboard랑 연동이 제대로 되지 않는 잇슈가 생겨서, init.lua에서 if else 처리를 제대로 안한 게 문제인 것 같아서, 수정했습니다.
    수정하니 잘 작동합니다.

--- init.lua
+++ init.lua
@@ -1,22 +1,23 @@
 if vim.g.vscode then
   -- https://github.com/vscode-neovim/vscode-neovim/issues/298
   vim.opt.clipboard:append("unnamedplus")
-end
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
-require("lazy").setup("plugins")
+else
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
 
+  require("vim-options")
+  require("lazy").setup("plugins")
+end
 
 
