956515ef2b0d037e89fc550368cf9098e502f2f9
Gyunseo Lee <rbstj0311@naver.com>
2024-02-01 21:55:39 +0900

    fix: change treesitter option from ensure_installed to auto_install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,15 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {
-				"lua",
-				"rust",
-				"c",
-				"cpp",
-				"go",
-				"javascript",
-				"html",
-			},
+			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
