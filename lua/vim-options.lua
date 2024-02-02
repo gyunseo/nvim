0118e664f13b8730b8f9e0e30d4c8b04a29b11d4
Gyunseo Lee <rbstj0311@naver.com>
2024-02-02 20:18:54 +0900

    vim-options: change language to en_US.UTF-8 to support unicode yank

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.wo.number = true
 vim.g.mapleader = " "
-vim.api.nvim_exec("language en_US", true)
-vim.api.nvim_set_option("clipboard", "unnamed") -- to use OS clipboard
+vim.api.nvim_exec("language en_US.UTF-8", true) -- 한글 unicode를 yank하기 위해
+vim.api.nvim_set_option("clipboard", "unnamedplus") -- to use OS clipboard
 vim.o.exrc = true
 vim.o.secure = true
