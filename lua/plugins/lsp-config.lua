717ac1de6bb245e1d64c8a774457d17c517ea157
Gyunseo Lee <rbstj0311@naver.com>
2024-03-12 15:07:07 +0900

    lsp-config: add pyright

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,7 @@ return {
 					"lua_ls",
 					"tsserver",
 					"clangd",
+					"pyright",
 				},
 			})
 		end,
@@ -46,6 +47,7 @@ return {
 			})
 			lspconfig.tsserver.setup({})
 			lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
